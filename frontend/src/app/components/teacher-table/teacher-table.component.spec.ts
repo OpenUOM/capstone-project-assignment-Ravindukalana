bc8ba02401b1af69c12eae407e2626e750e4e5a5
Ravindukalana <pkalana458@gmail.com>
2023-05-27 22:46:42 +0530

    All function changes

--- frontend/src/app/components/teacher-table/teacher-table.component.spec.ts
+++ frontend/src/app/components/teacher-table/teacher-table.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TeacherTableComponent } from "./TeacherTableComponent";
+import { TeacherTableComponent } from './TeacherTableComponent';
 
 describe('TeacherTableComponent', () => {
   let component: TeacherTableComponent;
